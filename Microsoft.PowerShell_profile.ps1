5aa5359870e7fe313f8e66901855e49758b7c2a1
MediocreMitch <mediocremitch@gmail.com>
2024-11-13 06:50:58 -0600

    changed posh theme to darkblood

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,3 @@
 $env:XDG_CONFIG_HOME = "$HOME\.config"
 $env:Path += ";C:\Users\user\AppData\Local\Programs\oh-my-posh\bin"
-oh-my-posh init pwsh --config ~/Documents\Powershell\posh\plague.omp.json | Invoke-Expression
+oh-my-posh init pwsh --config ~/Documents\Powershell\posh\darkblood.omp.json | Invoke-Expression
